6af658e32e1f027bbd9e1ed72506b3d88463e058
Suryaprakash006 <102574671+Suryaprakash006@users.noreply.github.com>
2023-09-30 16:45:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ const mongoose = require('mongoose');
 const Comment = require('./models/comments');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const http = require('http'); // Import the 'http' module
+const { Server } = require('socket.io'); // Import 'socket.io'
+
 const app = express();
-require("dotenv").config();
+require('dotenv').config();
 app.use(cors());
-mongoose.connect('mongodb+srv://rajessh781:R%40jesh2512@personal-blog.dtfxubi.mongodb.net/CodeBlog', { useNewUrlParser: true });
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+
+mongoose.connect('mongodb+srv://rajessh781:R%40jesh2512@personal-blog.dtfxubi.mongodb.net/CodeBlog', {
+  useNewUrlParser: true,
+});
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 mongoose.set('strictQuery', true);
+
+// Create an HTTP server using Express app
+const server = http.createServer(app);
+
+// Create a WebSocket server attached to the HTTP server
+const io = new Server(server);
 
 // Define a Comment model (assuming it's already defined)
 
@@ -26,13 +39,16 @@ app.post('/Sensor/add/:commentId', async (req, res) => {
       // If the comment with the given ID exists, update its data
       existingComment.data = data;
       await existingComment.save();
-      res.send("Comment Updated");
+      res.send('Comment Updated');
     } else {
       // If the comment with the given ID doesn't exist, create a new comment
       const newComment = new Comment({ _id: commentId, data });
       await newComment.save();
-      res.send("Comment Created");
+      res.send('Comment Created');
     }
+
+    // Emit an event to notify clients about the update
+    io.emit('commentUpdate', { commentId, data });
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
@@ -60,12 +76,26 @@ app.delete('/Sensor/delete/:commentId', async (req, res) => {
       return res.status(404).json({ message: 'Comment not found' });
     }
 
+    // Emit an event to notify clients about the deletion
+    io.emit('commentDeleted', { commentId });
     res.status(200).json({ message: 'Comment deleted successfully', deletedComment });
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-app.listen(process.env.PORT || 3001, () => {
-  console.log("You're Connected");
+// Listen on the same port as the Express app
+const port = process.env.PORT || 3001;
+server.listen(port, () => {
+  console.log(`You're Connected on port ${port}`);
+});
+
+// Set up WebSocket event handling
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
 });
